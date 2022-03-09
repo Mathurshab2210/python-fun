74cb558d8ed0fc8d78f95acf0571b6c05f93bf04
Nikhil Mathur <86379900+Mathurshab2210@users.noreply.github.com>
2022-03-09 18:57:00 +0530

    Update select.py
    I have created a random name selector from given n names it selects a name randomly

--- select.py
+++ select.py
@@ -5,5 +5,5 @@ for i in range(0,n):
     list1.append(input("enter friends name"))
 
 m=random.randint(0,n)
-print(f"{list1[m]} will give party today")
+print(f"{list1[m]} will give party today");
      
