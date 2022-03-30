9e1285d385d53389b37083d277931ab3422c07a1
Nikhil Mathur <86379900+Mathurshab2210@users.noreply.github.com>
2022-03-31 00:02:09 +0530

    password generator
    I have created a password generator which is very precize and ask for how many symbols words and numbers you want and then generate a random password

---
+++ passsword_generator.py
@@ -0,0 +1,27 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password=[]
+str1=""
+
+for char in range(1,nr_letters+1):
+    new=random.choice(letters)
+    password.append(new)
+    
+for num in range(1,nr_numbers+1):
+    numnew=random.choice(numbers)
+    password.append(numnew)
+    
+for sym in range(1,nr_symbols+1):
+    symnew=random.choice(symbols)
+    password.append(symnew)
+    
+random.shuffle(password)
+print(str1.join(password))
+    
