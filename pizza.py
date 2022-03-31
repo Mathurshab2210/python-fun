7f0205b38bdb69a926988ec70e5570b8b4e28d5b
Nikhil Mathur <86379900+Mathurshab2210@users.noreply.github.com>
2022-04-01 00:35:01 +0530

    Random_person_selector
    I have created an algorithm that will give you the name of bill payee person on the basis of the salary of the person or amount at that time person have it works on a given threshold that is 1500 rs

---
+++ pizza.py
@@ -0,0 +1,24 @@
+import random
+list1=[]
+n=int(input("Enter number of friends"))
+for i in range(0,n):
+    list1.append(input("enter friends name"))
+list2=[]
+for i in range(n):
+    list2.append(int(input(f'enter salary of {i} friend')))
+    
+m=random.randint(0,n-1)
+print(list2[m])
+end=False
+while not end:
+    if list2[m]>1500:
+        print(f'{list1[m]} will give party today')
+        end=True
+    else:
+        q=random.randint(0,n-1)
+        print(f'{list1[q]} will give party today')
+        end=True
+       
+    
+    
+     
