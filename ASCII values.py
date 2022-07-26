a496c79112b23ed039ba5483ca10e715fb96c634
Nikhil Mathur <86379900+Mathurshab2210@users.noreply.github.com>
2022-07-26 13:52:28 +0530

    Add files via upload

---
+++ ASCII values.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 11:37:37 2020
+
+@author: Nikhil Mathur
+"""
+
+
+c=input("Enter character for ASCII value:m")
+print("ASCII value of" +c+ "is", ord(c))
